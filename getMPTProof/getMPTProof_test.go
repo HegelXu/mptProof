260ed0f06b888c34d1309530e608e3975ff813fc
hegel <hegel@example.com>
2023-02-07 14:45:10 +0800

    new file:   getMPTProof/getMPTProof_test.go

---
+++ getMPTProof/getMPTProof_test.go
@@ -0,0 +1,69 @@
+package getmptproof_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	getmptproof "github.com/hegelXu/GetMerkleProof/getMPTProof"
+	verifymptproof "github.com/hegelXu/GetMerkleProof/verifyMPTProof"
+)
+
+// src: https://chainlist.org/chain/1
+var URL = []string{
+	"https://rpc.payload.de",
+	"https://rpc.builder0x69.io",
+	"https://ethereum.publicnode.com",
+	"https://endpoints.omniatech.io/v1/eth/mainnet/public",
+	"https://1rpc.io/eth",
+}
+
+func TestGetMerkleProof(t *testing.T) {
+	var client *ethclient.Client
+	var err error
+	for _, url := range URL {
+		client, err = ethclient.Dial(url)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			break
+		}
+	}
+	defer client.Close()
+
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	block, err := client.BlockByNumber(context.Background(), header.Number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	txIndex := rand.Intn(len(block.Transactions()))
+	tx := block.Body().Transactions[txIndex]
+	proof, err := getmptproof.GetMerkleProof(block, tx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("-----Test the correct proof:-----")
+	if !verifymptproof.VerifyMerkleProof(tx, block.TxHash(), *proof) {
+		fmt.Println("Verification failed.")
+	} else {
+		fmt.Println("Verification succeeded.")
+	}
+
+	fmt.Print("-----Test incorrect proof:-----")
+	proof.Value = append(proof.Value, '!')
+	if !verifymptproof.VerifyMerkleProof(tx, block.TxHash(), *proof) {
+		fmt.Println("Incorrect MPT Proof.")
+	} else {
+		fmt.Println("wrong!")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -12,10 +12,12 @@ require (
 	github.com/VictoriaMetrics/fastcache v1.6.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -25,4 +27,5 @@ require (
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
